7dbb8dd0efeeeb9e29eaa63c904ed9ee706b02f9
John Doe <no-email@gmail.com>
2019-02-22 19:52:38 +0200

    Fixed lint errors

--- src/app/shared/form-helpers/error-state-matcher.ts
+++ src/app/shared/form-helpers/error-state-matcher.ts
@@ -1,8 +1,8 @@
 import {ErrorStateMatcher} from '@angular/material/core';
-import {FormControl, FormGroupDirective, NgForm} from '@angular/forms';
+import {FormControl, FormGroupDirective, NgForm, AbstractControl} from '@angular/forms';
 
 export class CustomErrorStateMatcher implements ErrorStateMatcher {
-    isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    isErrorState(control: FormControl | AbstractControl | null, form: FormGroupDirective | NgForm | null): boolean {
         const isSubmitted = form && form.submitted;
         return !!(control && control.invalid && (control.touched || isSubmitted));
     }
